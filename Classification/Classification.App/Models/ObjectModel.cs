64d9f2a65e6d45670270bddc3c51658a64f6a6a6
Quisy <lamprecht.mariusz@gmail.com>
2017-05-29 22:08:10 +0200

    Added Database and ObjectModel classes

---
+++ Classification/Classification.App/Models/ObjectModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Classification.App.Models
+{
+    public class ObjectModel
+    {
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+        public IList<float> Features { get; set; }
+
+        public int FeaturesNumber => Features.Count;
+
+        public ObjectModel(string className, IList<float> features)
+        {
+            ClassId = -1;
+            ClassName = className;
+            Features = features;
+        }
+    }
+}
---
+++ Classification/Classification.App/Utils/DataBase.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Classification.App.Models;
+
+namespace Classification.App.Utils
+{
+    public class Database
+    {
+        public IList<ObjectModel> Objects { get; private set; }
+        public Dictionary<string, int> ClassCounters { get; set; }
+        public IList<string> ClassNames { get; set; }
+        public IList<int> FeaturesIDs { get; set; }
+        public int NoClass => ClassNames.Count;
+        public int NoObjects => Objects.Count;
+        public int NoFeatures => FeaturesIDs.Count;
+
+        public Database()
+        {
+            Objects = new List<ObjectModel>();
+            ClassCounters = new Dictionary<string, int>();
+            ClassNames = new List<string>();
+            FeaturesIDs = new List<int>();
+        }
+
+        public bool AddObject(ObjectModel obj)
+        {
+            if (NoFeatures > 0 && NoFeatures != obj.FeaturesNumber)
+                return false;
+
+            Objects.Add(obj);
+
+            //if (ClassCounters[obj.ClassName]++ == 0)
+            //    ClassNames.Add(obj.ClassName);
+
+            return true;
+        }
+
+        public bool Load(string fileName)
+        {
+            this.Clear();
+
+            var lines = File.ReadAllLines(fileName);
+            if (lines.Length == 0)
+                return false;
+
+            string firstLine = lines[0];
+
+            int classFeaturesNumber = int.Parse(firstLine.Substring(0, firstLine.IndexOf(',')));
+
+            int temppos = firstLine.IndexOf(',');
+
+            string featuresIds = firstLine.Substring(temppos + 1);
+            FeaturesIDs = featuresIds.Split(',').Select(int.Parse).ToList();
+
+            lines = lines.Where((val, idx) => idx > 0).ToArray();
+
+            foreach (string line in lines)
+            {
+                int pos = line.IndexOf(',');
+                string className = line.Substring(0, pos);
+                int classNamePos = className.IndexOf(' ');
+                className = className.Substring(0, classNamePos);
+
+                string features = line.Substring(pos + 1);
+                var featuresValues = features.Split(',').Select(y=>float.Parse(y.Replace('.',','))).ToList();
+
+                if (classFeaturesNumber == featuresValues.Count)
+                {
+                    AddObject(new ObjectModel(className, featuresValues));
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void Clear()
+        {
+            Objects.Clear();
+            ClassCounters.Clear();
+            ClassNames.Clear();
+            FeaturesIDs.Clear();
+        }
+    }
+}
